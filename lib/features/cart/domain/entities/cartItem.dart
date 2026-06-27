b4fda5809b997dc0b4672da1c147c6aa88a1a278
Amer Kriany <amer.kriany0@gmail.com>
2026-06-27 10:55:18 +0300

    feat: add cart logic

---
+++ lib/features/cart/domain/entities/cartItem.dart
@@ -0,0 +1,29 @@
+class CartItem {
+  final String itemId;
+  final String nameAr;
+  final double price;
+  final int quantity;
+  final String businessId;
+  CartItem({
+    required this.itemId,
+    required this.nameAr,
+    required this.price,
+    required this.quantity,
+    required this.businessId,
+  });
+  CartItem copyWith({
+  String? itemId,
+  String? nameAr,
+  double? price,
+  int? quantity,
+  String? businessId,
+}) {
+  return CartItem(
+    itemId: itemId ?? this.itemId,
+    nameAr: nameAr ?? this.nameAr,
+    price: price ?? this.price,
+    quantity: quantity ?? this.quantity,
+    businessId: businessId ?? this.businessId,
+  );
+}
+}
