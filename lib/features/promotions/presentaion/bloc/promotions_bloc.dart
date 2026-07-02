cff1055ad8b1dc109ad9c3ed7520e79e47b1175a
Amer Kriany <amer.kriany0@gmail.com>
2026-07-02 10:56:01 +0300

    feat: implement promotions use case and update promotions bloc with loading and error states

--- lib/features/promotions/presentaion/bloc/promotions_bloc.dart
+++ lib/features/promotions/presentaion/bloc/promotions_bloc.dart
@@ -1,3 +1,5 @@
+import 'package:aklatna/features/promotions/domain/entities/promotionEntity.dart';
+import 'package:aklatna/features/promotions/domain/usecases/promotionsUseCase.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
@@ -5,9 +7,22 @@ part 'promotions_event.dart';
 part 'promotions_state.dart';
 
 class PromotionsBloc extends Bloc<PromotionsEvent, PromotionsState> {
-  PromotionsBloc() : super(PromotionsInitial()) {
-    on<PromotionsEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final Promotionsusecase promotionsusecase;
+  PromotionsBloc({required this.promotionsusecase})
+    : super(PromotionsInitial()) {
+    on<PromotionsEvent>(_getAllPromotions);
+  }
+  // get all promotions
+  Future<void> _getAllPromotions(
+    PromotionsEvent event,
+    Emitter<PromotionsState> emit,
+  ) async {
+    try {
+      emit(PromotionsLoading());
+      final promotions = await promotionsusecase();
+      emit(PromotionsLoaded(promotions: promotions));
+    } catch (e) {
+      emit(PromotionsError(message: e.toString()));
+    }
   }
 }
--- lib/features/promotions/presentaion/bloc/promotions_state.dart
+++ lib/features/promotions/presentaion/bloc/promotions_state.dart
@@ -2,9 +2,23 @@ part of 'promotions_bloc.dart';
 
 sealed class PromotionsState extends Equatable {
   const PromotionsState();
-  
+
   @override
   List<Object> get props => [];
 }
 
 final class PromotionsInitial extends PromotionsState {}
+
+final class PromotionsLoading extends PromotionsState {}
+
+final class PromotionsLoaded extends PromotionsState {
+  final List<Promotionentity> promotions;
+  const PromotionsLoaded({required this.promotions});
+  @override
+  List<Object> get props => [promotions];
+}
+
+final class PromotionsError extends PromotionsState {
+  final String message;
+  const PromotionsError({required this.message});
+}
