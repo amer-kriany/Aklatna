5e654da391d32c5284345da1f6eadab84976055a
Amer Kriany <amer.kriany0@gmail.com>
2026-08-11 18:56:31 +0300

    fix some bugs

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -49,6 +49,8 @@ android {
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("release")
+        isMinifyEnabled = false
+        isShrinkResources = false
         }
     }
 }
